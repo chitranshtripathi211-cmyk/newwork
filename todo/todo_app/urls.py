be3a2046a2214042daf18c3cd8ad8807e45f577c
chitranshtripathi211-cmyk <chitranshtripathi211@gmail.com>
2026-03-15 13:58:59 +0530

    In this stage i have added the  task_display.html file make it working app with delete functionality for task

--- todo/todo_app/urls.py
+++ todo/todo_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path 
-from .views import home
+from .views import home ,taskDisplay, deleteTask
+
 
 urlpatterns = [path('', home, name='home'),
+        path('task', taskDisplay, name='task'),
+        path('delete/<int:id>', deleteTask, name='taskDelete')
 ]
--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import Todo
+
+from django.shortcuts import redirect, render
+from .models import Todo
 
 # Create your views here. we rite here logic  our application
 def home(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        completed = request.POST.get('completed') == 'on'  # Checkbox returns 'on' when checked
+        deadline = request.POST.get('deadline')
+        created_at = request.POST.get('created_at')
+        task = Todo(title=title, description=description, completed=completed, deadline=deadline, created_at=created_at)
+        task.save()# Redirect to the home page after saving the task
+        return redirect('home')
     return render(request, 'home.html')
+ # Render the home page template for GET requests
+
+
+
+def taskDisplay(request):
+    task = Todo.objects.all()  # Retrieve all tasks from the database
+
+    return render(request, 'task_display.html', {'task': task})  # Pass the tasks to the template for rendering
+
+
+
+def deleteTask(request, id):
+    task = Todo.objects.get(id=id)
+    task.delete()
+    return redirect('task')
